1652ab01819773a8333c50f712b1368d2aea4d87
Guilherme Moraes <113704939+guimoraes021@users.noreply.github.com>
2022-12-15 11:43:18 -0300

    adicionei o footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 import "./App.css";
+import Footer from "./components/rodape";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
